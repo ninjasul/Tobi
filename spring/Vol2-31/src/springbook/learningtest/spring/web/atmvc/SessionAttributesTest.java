46f514180749b678671b86c95d492568793ebb97
1000141 <1000141@ONE1000141PN001.onestorecorp.com>
2017-02-14 15:47:20 +0900

    테스트 객체 추가

--- spring/Vol2-31/src/springbook/learningtest/spring/web/atmvc/SessionAttributesTest.java
+++ spring/Vol2-31/src/springbook/learningtest/spring/web/atmvc/SessionAttributesTest.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpSession;
 
 import org.junit.Test;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,6 +33,9 @@ public class SessionAttributesTest extends AbstractDispatcherServletTest {
 		// 모델로 리턴된 user와 HttpSession에 저장된 user가 동일한 오브젝트인지 확인
 		assertThat(session.getAttribute("user"), is(getModelAndView().getModel().get("user")));
 		
+		// 모델로 리턴된 order와 HttpSession에 저장된 order가 동일한 오브젝트인지 확인
+		assertThat(session.getAttribute("order"), is(getModelAndView().getModel().get("order")));
+		
 		initRequest("/user/edit", "POST").addParameter("id", "1").addParameter("name", "Spring2");
 		
 		// 이전 요청을 세션을 가져와 설정하여 세션 상태가 유지된 채로 다음 요청을 보내도록 함.
@@ -43,13 +47,17 @@ public class SessionAttributesTest extends AbstractDispatcherServletTest {
 		
 		// SessionStatus 를 통해 세션에 저장된 user가 제거됐는지 확인
 		assertThat(session.getAttribute("user"), is(nullValue()));
+		
+		// SessionStatus 를 통해 세션에 저장된 order가 제거됐는지 확인
+		assertThat(session.getAttribute("order"), is(nullValue()));
 	}
 	
 	@Controller	
-	@SessionAttributes("user")
+	@SessionAttributes({"user", "order"})
 	static class UserController {
 		@RequestMapping(value="/user/edit", method=RequestMethod.GET) 
-		public User form(@RequestParam int id) {
+		public User form(@RequestParam int id, Model model) {
+			model.addAttribute("order", new Order(id, 1, 5));
 			return new User(1, "Spring", "mail@spring.com");
 		}
 		
@@ -70,5 +78,18 @@ public class SessionAttributesTest extends AbstractDispatcherServletTest {
 		public String getEmail() { return email; }
 		public void setEmail(String email) { this.email = email; }
 		public String toString() { return "User [email=" + email + ", id=" + id + ", name=" + name + "]";	}		
+	}
+	
+	// 테스트 용 주문 객체 
+	static class Order {
+		int userId; int productId; int quantity;			
+		public Order(int userId, int productId, int quantity) { this.userId = userId; this.productId = productId; this.quantity = quantity; }
+		public int getUserId() { return userId; }
+		public void setUserId(int userId) { this.userId = userId; }
+		public int getProductId() { return productId; }
+		public void setProductId(int productId) { this.productId = productId; }
+		public int getQuantity() { return quantity; }
+		public void setQuantity(int quantity) { this.quantity = quantity; }
+		public String toString() { return "User [userId=" + userId + ", productId=" + productId + ", quantity=" + quantity + "]";	}		
 	}
 }
